027d4707c00e9f376575da8bf21dab8baadb08de
ojedam13 <m_ojeda17@hotmail.com>
2021-08-12 18:02:20 -0300

    function agregarCurso

---
+++ js/app.js
@@ -0,0 +1,24 @@
+// Variables
+
+const carrito = document.querySelector('#carrito');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const vaciaCarritoBtn = document.querySelector('#vaciar-carrito');
+const listaCursos = document.querySelector('#lista-cursos');
+
+cargarEventListeners();
+function cargarEventListeners() {
+    //Cuando agregar un curso presionando "agregar al carrito"
+    listaCursos.addEventListener('click', agregarCurso);
+
+}
+
+
+// Funciones
+
+function agregarCurso(e) {
+    e.preventDefault();
+    if (e.target.classList.contains('agregar-carrito')) {
+         console.log(e.target)
+    }
+   
+}
