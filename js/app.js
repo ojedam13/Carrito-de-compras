13b70b05654fab1366c939dab8f68bda3c48f745
ojedam13 <m_ojeda17@hotmail.com>
2021-08-12 19:31:28 -0300

    leer datos del curso que seleccionamos

--- js/app.js
+++ js/app.js
@@ -18,7 +18,27 @@ function cargarEventListeners() {
 function agregarCurso(e) {
     e.preventDefault();
     if (e.target.classList.contains('agregar-carrito')) {
-         console.log(e.target)
+        const cursoSeleccionado = e.target.parentElement.parentElement;
+
+        leerDatosCurso(cursoSeleccionado);
     }
    
+}
+
+// lee el contenido del html al q le dimos click y extrae la info del curso
+
+function leerDatosCurso(curso) {
+    // console.log(curso);
+
+    //crear objeto con el contenido del curso actual
+    const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        id: curso.querySelector('a').getAttribute('data-id'),
+        cantidad: 1
+
+    }
+
+    console.log(infoCurso);
 }
