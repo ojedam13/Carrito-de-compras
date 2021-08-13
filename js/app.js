59c4339e2acfe67195b2f3bbc2c7db9c67906015
ojedam13 <m_ojeda17@hotmail.com>
2021-08-13 16:17:13 -0300

    mostrar el resto de la info en el carrito

--- js/app.js
+++ js/app.js
@@ -58,10 +58,23 @@ function carritoHtml() {
 
     //recorre el carrito y genera el html
     articulosCarrito.forEach(curso => {
+        const {imagen, titulo, precio, cantidad, id } = curso;
         const row = document.createElement('tr');
         row.innerHTML = `
             <td>
-                ${curso.titulo}
+               <img src = "${imagen}" width="100">
+            </td>
+            <td>
+                ${titulo}
+            </td>
+            <td>
+                ${precio}
+            </td>
+            <td>
+                ${cantidad}
+            </td>
+            <td>
+                <a href="#" class="borrar-curso" data-id="${id}"> X </a>
             </td>
         `;
 
